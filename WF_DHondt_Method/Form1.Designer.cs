91c5355faf34baa37806c42b974069b8aec35fa3
Jakub Syrek <jaqb.syrek@gmail.com>
2019-10-15 01:36:44 +0200

    1

--- WF_DHondt_Method/Form1.Designer.cs
+++ WF_DHondt_Method/Form1.Designer.cs
@@ -28,13 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tb_NumberOfParties = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_NumberOfMandates = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bt_GetMandatesOfEachParty = new System.Windows.Forms.Button();
+            this.tb_Output = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tb_NumberOfParties
+            // 
+            this.tb_NumberOfParties.Location = new System.Drawing.Point(34, 35);
+            this.tb_NumberOfParties.Name = "tb_NumberOfParties";
+            this.tb_NumberOfParties.Size = new System.Drawing.Size(147, 20);
+            this.tb_NumberOfParties.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NumberOfParties";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NumberOfMandates";
+            // 
+            // tb_NumberOfMandates
+            // 
+            this.tb_NumberOfMandates.Location = new System.Drawing.Point(34, 90);
+            this.tb_NumberOfMandates.Name = "tb_NumberOfMandates";
+            this.tb_NumberOfMandates.Size = new System.Drawing.Size(147, 20);
+            this.tb_NumberOfMandates.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bt_GetMandatesOfEachParty);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tb_NumberOfParties);
+            this.groupBox1.Controls.Add(this.tb_NumberOfMandates);
+            this.groupBox1.Location = new System.Drawing.Point(23, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(216, 181);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // bt_GetMandatesOfEachParty
+            // 
+            this.bt_GetMandatesOfEachParty.Location = new System.Drawing.Point(34, 127);
+            this.bt_GetMandatesOfEachParty.Name = "bt_GetMandatesOfEachParty";
+            this.bt_GetMandatesOfEachParty.Size = new System.Drawing.Size(147, 23);
+            this.bt_GetMandatesOfEachParty.TabIndex = 4;
+            this.bt_GetMandatesOfEachParty.Text = "GetVotesOfEachParty";
+            this.bt_GetMandatesOfEachParty.UseVisualStyleBackColor = true;
+            this.bt_GetMandatesOfEachParty.Click += new System.EventHandler(this.bt_GetMandatesOfEachParty_Click);
+            // 
+            // tb_Output
+            // 
+            this.tb_Output.Location = new System.Drawing.Point(6, 25);
+            this.tb_Output.Multiline = true;
+            this.tb_Output.Name = "tb_Output";
+            this.tb_Output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_Output.Size = new System.Drawing.Size(227, 149);
+            this.tb_Output.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tb_Output);
+            this.groupBox2.Location = new System.Drawing.Point(245, 23);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(250, 180);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tb_NumberOfParties;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_NumberOfMandates;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bt_GetMandatesOfEachParty;
+        private System.Windows.Forms.TextBox tb_Output;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- WF_DHondt_Method/Form1.cs
+++ WF_DHondt_Method/Form1.cs
@@ -16,5 +16,102 @@ namespace WF_DHondt_Method
         {
             InitializeComponent();
         }
+
+        int[] mandates;
+        int[] votes;
+        float[] calcTab;
+        TextBox[] textBoxes;
+        int partiesAmount;
+        int mandatesAmount;
+        int maxind;
+        float max;
+
+        private void bt_GetMandatesOfEachParty_Click(object sender, EventArgs e)
+        {
+            partiesAmount = 0;
+            mandatesAmount = 0;
+            int.TryParse(tb_NumberOfParties.Text, out partiesAmount);
+            int.TryParse(tb_NumberOfMandates.Text, out mandatesAmount);
+
+            if (partiesAmount > 0 && mandatesAmount > 0)
+            {
+                var x = groupBox1.Location.X ;
+                var y = groupBox1.Location.Y + groupBox1.Height;
+                mandates = new int[partiesAmount];
+                votes = new int[partiesAmount];
+                calcTab = new float[partiesAmount];
+
+                textBoxes = new TextBox[partiesAmount];
+                Label[] labels = new Label[partiesAmount];
+
+                for (int i = 0; i < textBoxes.Length; i++)
+                {
+                    labels[i] = new Label();
+                    labels[i].Location = new Point( x, y+=20);
+                    labels[i].Text = $"Party nr{i + 1}";
+                    textBoxes[i] = new TextBox();
+                    textBoxes[i].Name = $"Party{i + 1}";
+                    textBoxes[i].Location = new Point(x, y += 20);                    
+                }
+                for (int i = 0; i < textBoxes.Length; i++)
+                {
+                    this.Controls.Add(labels[i]);
+                    this.Controls.Add(textBoxes[i]);
+                }
+
+                Button button = new Button();
+                button.Location = new Point(x, y += 20);
+                button.Text = $"Get data to matrix analysis";
+                button.Click += new EventHandler(getDataForAnalysis);
+                this.Controls.Add(button);
+            }
+        }
+
+        private static float calcNewValue(int votes, int mandates)
+        {
+            return (float)((votes * 1.0) / (mandates + 1.0));
+        }
+
+        protected void getDataForAnalysis(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            for (int i = 0; i < textBoxes.Length; i++)
+            {
+                int vote = 0;
+                TextBox item = (TextBox)textBoxes[i];
+                int.TryParse(item.Text, out vote);
+                votes[i] = vote;
+            }            
+
+            for (int i = 0; i < partiesAmount; i++)
+            {
+                mandates[i] = 0;
+            }
+
+            for (int i = 0; i < partiesAmount; i++)
+            {
+                calcTab[i] = votes[i];
+            }
+            for (int i = 0; i < mandatesAmount; i++)
+            {
+                max = -1;
+                for (int j = 0; j < partiesAmount; j++)
+                {
+                    if(max < calcTab[j])
+                    {
+                        max = calcTab[j];
+                        maxind = j;
+                    }
+                }
+                mandates[maxind]++;
+                calcTab[maxind] = calcNewValue(votes[maxind], mandates[maxind]);
+            }
+            for (int i = 0; i < partiesAmount; i++)
+            {
+                tb_Output.AppendText($"Party{i + 1} has {mandates[i]} mandates{Environment.NewLine}");                
+            }
+        }
     }
+    
+
 }
